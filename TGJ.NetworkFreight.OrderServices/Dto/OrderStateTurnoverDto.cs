2d55696f1b2551aa1b71e59326f436b86f908581
tgj1qaz2wsx <Administrator@DESKTOP-MM8QI7M>
2020-11-30 18:02:38 +0800

    Dto/OrderStateTurnoverDto.cs

---
+++ TGJ.NetworkFreight.OrderServices/Dto/OrderStateTurnoverDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TGJ.NetworkFreight.OrderServices.Dto
+{
+    /// <summary>
+    /// 司机端我的Dto
+    /// </summary>
+    public class OrderStateTurnoverDto
+    {
+        /// <summary>
+        /// 调度中金额
+        /// </summary>
+        public decimal DispatchTurnover { get; set; }
+
+        /// <summary>
+        /// 已完成金额
+        /// </summary>
+        public decimal CompleteTurnover { get; set; }
+    }
+}

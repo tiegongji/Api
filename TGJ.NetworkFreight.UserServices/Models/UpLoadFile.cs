700ea68443b286a3fade83f05e79df32ec74a156
ygk688 <ygk688@gmail.com>
2020-12-11 09:55:21 +0800

    用户反馈

---
+++ TGJ.NetworkFreight.UserServices/Models/UpLoadFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TGJ.NetworkFreight.UserServices.Models
+{
+    /// <summary>
+    /// 文件存储
+    /// </summary>
+    public class UpLoadFile
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        [Key]
+        public int ID { set; get; }
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public int UserID { set; get; }
+        /// <summary>
+        ///  类型
+        /// </summary>
+        public int Type { set; get; }
+        /// <summary>
+        /// 关联ID
+        /// </summary>
+        public int TypeID { set; get; }
+        /// <summary>
+        ///  文件路径
+        /// </summary>
+        public string FilePath { set; get; }
+        /// <summary>
+        /// 创建日期
+        /// </summary>
+        public DateTime CreateTime { set; get; }
+    }
+}
---
+++ TGJ.NetworkFreight.UserServices/Services/IFeedBackService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TGJ.NetworkFreight.UserServices.Models;
+
+namespace TGJ.NetworkFreight.UserServices.Services
+{
+    public interface IFeedBackService
+    {
+        void Add(FeedBackDto entity);
+    }
+}
